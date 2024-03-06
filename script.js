dd0aa82d5c9f6be1a215526c2ad5a5f68b806d20
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-03-06 21:36:57 +0800

    Adjusted card select interaction: 2nd card cannot be selected before an operator is selected

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const cardList = document.querySelector('#card-btn');
 const operatorList = document.querySelector('#operator-btn');
 const operators = document.querySelectorAll('.operator');
 let selectedCounter = 0;
+let isOperatorSelected = false;
 
 function onHoverOver(e) {
     if(!e.target.classList.contains('selected')) {
@@ -22,9 +23,16 @@ function onMouseLeave(e) {
 
 function onCardClick(e) {
     if(e.target.classList.contains('card') && !e.target.classList.contains('selected')) {
-        e.target.style.backgroundColor = '#90EE90';
-        e.target.classList.add('selected');
-        selectedCounter += 1;
+        if(!isOperatorSelected && selectedCounter > 0) {
+            alert('Please select an operator before selecting another card!');
+            return;
+        }
+        else {
+            e.target.style.backgroundColor = '#90EE90';
+            e.target.classList.add('selected');
+            selectedCounter += 1;
+        }
+        
     }
     else {
         e.target.style.backgroundColor = '';
@@ -39,10 +47,14 @@ function onOperatorClick(e) {
         if(!e.target.classList.contains('selected')) {
             e.target.style.backgroundColor = '#90EE90';
             e.target.classList.add('selected');
+            isOperatorSelected = true;
+            console.log(isOperatorSelected);
         }
         else {
             e.target.style.backgroundColor = '';
             e.target.classList.remove('selected');
+            isOperatorSelected = false;
+            console.log(isOperatorSelected);
         }
     }
 }
@@ -56,11 +68,12 @@ function randomCardValue() {;
 // De-selects operator is no card is selected
 function checkUI() {
     if(selectedCounter < 1) {
-        console.log(operators);
         operators.forEach(operator => {
             if(operator.classList.contains('selected')) {
                 operator.classList.remove('selected');
                 operator.style.backgroundColor = '';
+                isOperatorSelected = false;
+                console.log(isOperatorSelected);
             }
         })
     }
