fdcebcc18cc8c541d87ef381c5981438af936f18
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-03-06 21:07:17 +0800

    Operator buttons are able to be in a selected state

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.card');
 const cardList = document.querySelector('#card-btn');
 const operatorList = document.querySelector('#operator-btn');
 
+let selectedCounter = 0;
+
 function onHoverOver(e) {
     if(!e.target.classList.contains('selected')) {
         if(e.target.classList.contains('card') || e.target.classList.contains('operator')) {
@@ -22,11 +24,29 @@ function onCardClick(e) {
     if(e.target.classList.contains('card') && !e.target.classList.contains('selected')) {
         e.target.style.backgroundColor = '#90EE90';
         e.target.classList.add('selected');
-        console.log(e.target.innerText);
+        selectedCounter += 1;
+        // console.log(e.target.innerText);
+        console.log(selectedCounter);
     }
     else {
         e.target.style.backgroundColor = '';
         e.target.classList.remove('selected');
+        selectedCounter -= 1;
+        console.log(selectedCounter);
+    }
+}
+
+function onOperatorClick(e) {
+    if(selectedCounter >= 1) {
+        if(!e.target.classList.contains('selected')) {
+            e.target.style.backgroundColor = '#90EE90';
+            e.target.classList.add('selected');
+        }
+        else {
+            e.target.style.backgroundColor = '';
+            e.target.classList.remove('selected');
+        }
+
     }
 }
 
@@ -43,10 +63,13 @@ function init() {
     // Event listeners
     cardList.addEventListener('mouseover', onHoverOver);
     cardList.addEventListener('mouseout', onMouseLeave);
+    cardList.addEventListener('click', onCardClick);
+
     operatorList.addEventListener('mouseout', onMouseLeave);
     operatorList.addEventListener('mouseover', onHoverOver);
+    operatorList.addEventListener('click', onOperatorClick);
 
-    cardList.addEventListener('click', onCardClick);
+    
 }
 
 
