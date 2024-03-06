f2a6332ce9194bd240443e881bde293b5efc9f81
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-03-06 20:51:24 +0800

    Added hover effects on cards and operator buttons and selected state on cards

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-function solve() {
-    
+const cards = document.querySelectorAll('.card');
+const cardList = document.querySelector('#card-btn');
+const operatorList = document.querySelector('#operator-btn');
+
+function onHoverOver(e) {
+    if(!e.target.classList.contains('selected')) {
+        if(e.target.classList.contains('card') || e.target.classList.contains('operator')) {
+            e.target.style.backgroundColor = '#ccc';
+        }
+    }
 }
+
+function onMouseLeave(e) {
+    if(!e.target.classList.contains('selected')) {
+        if(e.target.classList.contains('card') || e.target.classList.contains('operator')) {
+            e.target.style.backgroundColor = '';
+        }
+    }
+}
+
+function onCardClick(e) {
+    if(e.target.classList.contains('card') && !e.target.classList.contains('selected')) {
+        e.target.style.backgroundColor = '#90EE90';
+        e.target.classList.add('selected');
+        console.log(e.target.innerText);
+    }
+    else {
+        e.target.style.backgroundColor = '';
+        e.target.classList.remove('selected');
+    }
+}
+
+
+function randomCardValue() {;
+    cards.forEach(card => 
+        card.innerText = Math.floor(Math.random() * 10 + 1    
+    ));
+}
+
+function init() {
+    randomCardValue();
+
+    // Event listeners
+    cardList.addEventListener('mouseover', onHoverOver);
+    cardList.addEventListener('mouseout', onMouseLeave);
+    operatorList.addEventListener('mouseout', onMouseLeave);
+    operatorList.addEventListener('mouseover', onHoverOver);
+
+    cardList.addEventListener('click', onCardClick);
+}
+
+
+init();
